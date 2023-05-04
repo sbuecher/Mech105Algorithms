e11e717c718bf4327e6858a8b8aa818f1690c0b0
sbuecher <sbuecher@colostate.edu>
2023-05-03 21:26:43 -0600

    Moved MATLAB files into proper folders and created README text for Simpson method

---
+++ Integration/README.m
@@ -0,0 +1,4 @@
+*** Integration Methods
+
+This section contains the code used for numerical integration methods, such as the Simpson method, Trapezoidal Method, etc.
+
---
+++ Integration/SimpMethod/README.m
@@ -0,0 +1,10 @@
+** MATLAB Simpson method code
+
+This code contains a MATLAB function that integrates a table of value using Simpson's method
+
+Numerical evaluation of integral by Simpson's 1/3 Rule
+ Inputs
+   x = the vector of equally spaced independent variable
+   y = the vector of function values with respect to x
+ Outputs
+   I = the numerical integral calculated
